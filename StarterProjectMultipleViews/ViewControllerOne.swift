d687f8693c2555a1ef58167f2cf7d02e9c414897
Isaac Ho <isaacho2028@cdnis.edu.hk>
2026-03-07 22:22:41 +0800

    Event Details Screen UI Framework
    - Setup the UI framework for the event details screen

--- StarterProjectMultipleViews/ViewControllerOne.swift
+++ StarterProjectMultipleViews/ViewControllerOne.swift
@@ -31,19 +31,82 @@ class ViewControllerOne: UIViewController {
     
     // MARK: - IBOutlets
     
-    
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var userSelection: UILabel!
     
     // MARK: - Variables and Constants
     
-    
+    var selectedInterest = false
     
     // MARK: - IBActions and Functions
+    
+    @IBAction func interestButton(_ sender: UIButton) {
+        
+        // Checking if the user has selected a button
+        
+        if sender.tag != 0 {
+            selectedInterest = true
+        }
+        
+        // Assigning the category as a text to a variable if the user has selected a button
+        
+        /*
+         If the user has selected a button:
+         - Use the button's tag to determine the category the user selected and assign it to the userInterest variable (this will be used later to automatically filter the homepage screen according to the category
+         - Using a label to tell the user what button they selected (for confirmation) using enums
+         */
+        
+        if selectedInterest == true {
+            
+            switch sender.tag {
+            case 1:
+                Data.userInterest = Data.categoryString(category: Data.category.education)
+            case 2:
+                Data.userInterest = Data.categoryString(category: Data.category.sports)
+            case 3:
+                Data.userInterest = Data.categoryString(category: Data.category.arts)
+            case 4:
+                Data.userInterest = Data.categoryString(category: Data.category.others)
+            default:
+                return
+            }
+            
+            userSelection.text = "Your Selection: \(Data.userInterest)"
+            
+        }
+    }
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        
+        // Checking if all inputs have been inputted (username and selected user interest)
+        
+        if userTextField.text != "" && selectedInterest == true {
+            
+            // Assigning the username to a global variable & moving to the next screen
+            
+            Data.username = userTextField.text!
+            
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainVCNav")
+            self.present(vc, animated: true, completion: nil)
+            
+        } else {
+            
+            // Showing the error message if inputs haven't been fully completed
+            
+            errorMessage.isHidden = false
+        }
+    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Hiding the error message by default
+        
+        errorMessage.isHidden = true
     }
-
 
 }
 
